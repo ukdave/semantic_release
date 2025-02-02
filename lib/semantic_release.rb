695ce3db7cb82519e5c58fca2a1e98f1f8de5ea7
David Bull <david@uk-dave.com>
2025-02-02 23:01:09 +0000

    Add top-level methods

--- lib/semantic_release.rb
+++ lib/semantic_release.rb
@@ -8,4 +8,40 @@ module SemanticRelease
   class Error < StandardError; end
 
   SEMVER_FILE = ".semver"
+
+  def self.init
+    version = Semver.new
+    version.save(SEMVER_FILE)
+  end
+
+  def self.current_version
+    Semver.load(SEMVER_FILE).to_s
+  end
+
+  def self.inc_major
+    increment(:major)
+    release
+  end
+
+  def self.inc_minor
+    increment(:minor)
+    release
+  end
+
+  def self.inc_patch
+    increment(:patch)
+    release
+  end
+
+  def self.increment(term)
+    version = Semver.load(SEMVER_FILE)
+    version.increment(term)
+    version.save
+  end
+
+  def self.release
+    Updaters::Changelog.update
+    Updaters::VersionRb.update
+    Updaters::GitTag.update
+  end
 end
